34768b2ed22152f816be5a0eabf8f30b527df909
anzelina <g00414991@atu.ie>
2025-03-25 00:45:56 +0000

    asking for username and password

--- src/main/java/ie/atu/OOP_Project/userLogin.java
+++ src/main/java/ie/atu/OOP_Project/userLogin.java
@@ -1,9 +1,7 @@
 package ie.atu.OOP_Project;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class userLogin {
@@ -15,6 +13,41 @@ public class userLogin {
         String username = null;
         String password = null;
 
+        try
+        {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(Selectsql);
+
+            while(resultSet.next())
+            {
+                username = resultSet.getString("username");
+                password = resultSet.getString("password");
+            }
+
+            System.out.println("====== LOGIN ======");
+            System.out.println("Do you have an account? (y/n)");
+            String answer = scanner.nextLine();
+
+            if(answer.equalsIgnoreCase("n"))
+            {
+                System.out.println("Create an account");
+                CreateAccount CreateAccount = new CreateAccount();
+                CreateAccount.createNewAccount();
+            }
+            else
+            {
+                System.out.println("Please enter username");
+                String inputUsername = scanner.nextLine();
+                System.out.println("Please enter password");
+                String inputPassword = scanner.nextLine();
+
+
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
 
     }
 }
