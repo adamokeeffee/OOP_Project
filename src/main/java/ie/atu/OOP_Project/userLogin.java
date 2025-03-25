d005b4ffb07e3f59a11a1cb68499cc82491d4e4f
anzelina <g00414991@atu.ie>
2025-03-25 00:38:21 +0000

    selecting data from database

--- src/main/java/ie/atu/OOP_Project/userLogin.java
+++ src/main/java/ie/atu/OOP_Project/userLogin.java
@@ -1,7 +1,20 @@
 package ie.atu.OOP_Project;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class userLogin {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql//localhost/database", "root", "password");
+        Scanner scanner = new Scanner(System.in);
+        String Selectsql = "SELECT u.username, u.password" + "FROM users u";
+
+        String username = null;
+        String password = null;
+
 
     }
 }
